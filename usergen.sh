e458f59a75db12afdc24ec2e2c048c4987955244
definitelyuncertain <saikirann94@gmail.com>
2017-10-20 14:18:42 +0000

    Update usergen.sh

--- usergen.sh
+++ usergen.sh
@@ -7,13 +7,13 @@ bash volumes.sh $1 $2
 
 bash keygen.sh $1
 
-sudo mkdir -p /home/$1/.kube/key
+sudo mkdir -p /storage/home/$1/.kube/key
 
-sudo cp ./key/*.key ./key/*.crt ./key/*.csr /home/$1/.kube/key/
+sudo cp ./key/*.key ./key/*.crt ./key/*.csr /storage/home/$1/.kube/key/
 
 sudo chown -R $1: /storage/home/$1/.kube
 
-kubectl config set-credentials $1 --client-certificate=/home/$1/.kube/key/user1.crt  --client-key=/home/$1/.kube/key/user1.key
+kubectl config set-credentials $1 --client-certificate=/storage/home/$1/.kube/key/user1.crt  --client-key=/storage/home/$1/.kube/key/user1.key
 
 kubectl config set-context $1-context --cluster=kubernetes --namespace=$1-namespace --user=$1
 
