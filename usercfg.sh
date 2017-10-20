ce9d753d17f4baf8cffbc64ddbe1b517bb1bd897
definitelyuncertain <saikirann94@gmail.com>
2017-10-20 14:20:03 +0000

    Update usercfg.sh

--- usercfg.sh
+++ usercfg.sh
@@ -1,6 +1,6 @@
 kubectl config set-cluster kubernetes --server=https://$2:6443 --certificate-authority=/etc/kubernetes/pki/apiserver.crt
 
-kubectl config set-credentials $1 --client-certificate=/home/$1/.kube/key/$1.crt  --client-key=/home/$1/.kube/key/$1.key
+kubectl config set-credentials $1 --client-certificate=/storage/home/$1/.kube/key/$1.crt  --client-key=/storage/home/$1/.kube/key/$1.key
 
 kubectl config set-context $1-context --cluster=kubernetes --namespace=$1-namespace --user=$1
 
