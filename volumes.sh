1afe34f6fca076a806115fdcc53c173174c99633
definitelyuncertain <saikirann94@gmail.com>
2017-10-20 19:19:06 +0000

    Update volumes.sh

--- volumes.sh
+++ volumes.sh
@@ -1,21 +1,15 @@
 # Provision NFS volumes
 
-python genpv.py $1-home $2 "/storage/home/$1"
+python genpv.py $1-home $2 "/storage/home/$1" ReadWriteOnce
 kubectl create -f manifests/pv-$1-home.yaml --namespace=$1-namespace
 
-python genpvc.py $1-home
+python genpvc.py $1-home ReadWriteOnce
 kubectl create -f manifests/pvc-$1-home.yaml --namespace=$1-namespace
 
 
-python genpv.py tools $2 "/tools"
-kubectl create -f manifests/pv-tools.yaml --namespace=$1-namespace
-
-python genpvc.py tools
+python genpvc.py tools ReadOnlyMany
 kubectl create -f manifests/pvc-tools.yaml --namespace=$1-namespace
 
 
-python genpv.py datasets $2 "/datasets"
-kubectl create -f manifests/pv-datasets.yaml --namespace=$1-namespace
-
-python genpvc.py datasets
+python genpvc.py datasets ReadOnlyMany
 kubectl create -f manifests/pvc-datasets.yaml --namespace=$1-namespace
