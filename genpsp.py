9ecd103fb142873cf90439a5b896d957c203c5a5
definitelyuncertain <saikirann94@gmail.com>
2017-10-20 11:42:22 +0000

    Restrict uid in psp

--- genpsp.py
+++ genpsp.py
@@ -1,6 +1,8 @@
 import sys, os
 import re
 
+uid = sys.argv[1]
+
 pspstring =\
 r'''apiVersion: extensions/v1beta1
 kind: PodSecurityPolicy
@@ -8,8 +10,10 @@ metadata:
   name: restrict-root
 spec:
   privileged: false
-  runAsUser:
-    rule: MustRunAsNonRoot
+    rule: MustRunAs
+    ranges:
+    - min: '''+uid+'''
+      max: '''+uid+'''
   seLinux:
     rule: RunAsAny
   fsGroup:
