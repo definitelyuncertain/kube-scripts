f82063e740bd4cc503be667a376c32e19d0e41c8
definitelyuncertain <saikirann94@gmail.com>
2017-10-20 13:16:51 +0000

    Create genpv.py

---
+++ genpv.py
@@ -0,0 +1,25 @@
+import sys, os
+import re
+
+name = sys.argv[1]
+ip = sys.argv[2]
+path = sys.argv[3]
+
+yamlstring =\
+r'''apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: nfs
+spec:
+  capacity:
+    storage: 6Gi
+  accessModes:
+    - ReadWriteOnce
+  nfs:
+    server: ''' + ip + '''
+    path: ''' + path + '''
+'''
+
+fh=open('manifests/pv-name.yaml','w')
+fh.write(yamlstring)
+fh.close()
